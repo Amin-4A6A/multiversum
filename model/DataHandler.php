9940fa4e7f7b6fcd78fcb204a193d8b15d9f4caa
Amin-4A6A <510074@edu.rocmn.nl>
2018-05-22 13:59:10 +0200

    datahandler

---
+++ model/DataHandler.php
@@ -0,0 +1,113 @@
+<?php
+
+class DataHandler {
+
+    public $pdo;
+
+    public $lastSelect = [];
+
+    public $host;
+    public $database;
+    public $username;
+    public $password;
+    public $dbtype;
+
+    public function __construct($host, $database, $username, $password, $dbtype = "mysql") {
+        try {
+            $this->pdo = new PDO("$dbtype:host=$host;dbname=$database", $username, $password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]);
+        } catch(PDOexeption $e) {
+            $this->showError("Error: " . $e->getMessage());
+        }
+
+        $this->host = $host;
+        $this->database = $database;
+        $this->username = $username;
+        $this->password = $password;
+        $this->dbtype = $dbtype;
+    }
+
+    public function createData($sql, $bindings = []) {
+        $sth = $this->pdo->prepare($sql);
+        $sth->execute($bindings);
+        return $this->pdo->lastInsertId();
+    }
+
+    public function readData($sql, $bindings = [], $multiple = true, $pagination = false) {
+
+        $sql = $sql
+                . ($pagination ? " LIMIT $pagination OFFSET " . (intval(($_REQUEST["page"] ?? 0)) * $pagination ?? 0) : "");
+
+        $sth = $this->pdo->prepare($sql);
+        $sth->execute($bindings);
+
+        $this->lastSelect = compact("bindings", "sql");
+
+        if($multiple) {
+            return $sth->fetchAll();
+        } else {
+            return $sth->fetch();
+        }
+        
+    }
+
+    public function updateData($sql, $bindings = []) {
+        $sth = $this->pdo->prepare($sql);
+        $sth->execute($bindings);
+        return $this->pdo->lastInsertId();        
+    }
+
+    public function deleteData($sql, $bindings = []) {
+        $sth = $this->pdo->prepare($sql);
+        return $sth->execute($bindings);
+    }
+
+    public function exportToCSV($data) {
+
+        function addQuotes($val) {
+            return "\"$val\"";
+        }
+
+        $csv = "";
+        foreach ($data as $value) {
+            $csv .= implode(", ", array_map("addQuotes", array_keys($value))) . "\r\n";
+            break;
+        }
+
+        foreach ($data as $value) {
+            $csv .= implode(", ", array_map("addQuotes", array_values($value))) . "\r\n";
+        }
+
+        echo $csv;
+    }
+
+    public function pagination($pagination) {
+
+        // remove "body" from select prev query
+        $sql = preg_split("/(?s)(?<=SELECT).*?(?=FROM)/", $this->lastSelect["sql"]);
+        // shuffle
+        $sql[2] = $sql[1];
+        // add count
+        $sql[1] = "COUNT(*) AS count";
+        // implode
+        $sql = implode($sql, " ");
+
+        $sql = preg_replace("/LIMIT [0-9]+ OFFSET [0-9]+/", "", $sql);
+                
+        $count =  $this->readData(
+            $sql,
+            $this->lastSelect["bindings"],
+            false
+        )["count"];
+
+        return ceil($count / $pagination);
+
+    }
+
+    public function showError($error) {
+        echo $error;
+    }
+
+}
