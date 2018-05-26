5194a2095aa40ee5b1d4f4add3112ff51aafef6d
leon in 't Veld <leon3110l@gmail.com>
2018-05-26 22:39:16 +0200

    added twig base files

--- controller/ProductController.php
+++ controller/ProductController.php
@@ -4,8 +4,12 @@ require "Controller.php";
 class ProductController extends Controller {
 
     public function handleRequest() {
-
         
+        switch ($_GET["op"] ?? false) {
+            default:
+                $this->render("layout/app.twig");
+                break;
+        }
 
     }
 
