e5069428229f7291e60898b19dfc2b9691d5e5ad
Amin-4A6A <510074@edu.rocmn.nl>
2018-05-27 18:43:17 +0200

    overview and contact

--- controller/ProductController.php
+++ controller/ProductController.php
@@ -4,8 +4,15 @@ require "Controller.php";
 class ProductController extends Controller {
 
     public function handleRequest() {
-        
+
         switch ($_GET["op"] ?? false) {
+
+          case 'contact':
+              $this->render("contact.twig");
+            break;
+            case 'overvieuw':
+                $this->render("overview.twig");
+              break;
             default:
                 $this->render("home.twig");
                 break;
