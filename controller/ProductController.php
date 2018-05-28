ed7c5e14b9de19fcb8169e88800e03ec48a148c0
Amin-4A6A <510074@edu.rocmn.nl>
2018-05-28 20:24:30 +0200

    styling

--- controller/ProductController.php
+++ controller/ProductController.php
@@ -7,6 +7,9 @@ class ProductController extends Controller {
 
         switch ($_GET["op"] ?? false) {
 
+          case 'home':
+              $this->render("home.twig");
+            break;
           case 'contact':
               $this->render("contact.twig");
             break;
