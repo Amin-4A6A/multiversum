7969cbc579e29ab06d0e467c8f4607ee67172c08
Amin-4A6A <510074@edu.rocmn.nl>
2018-06-16 19:17:25 +0200

    error solved

--- model/schoppingModel.php
+++ model/schoppingModel.php
@@ -10,30 +10,25 @@ require_once "DataHandler.php";
  */
 class schoppingCart {
 
-  public function addToProductCart($value='')
-  {
+  public function addToProductCart($value='') {
   // setcookie("schoppingCart", "0192371098237:1,0238409283409:1,02348092348:2");
 
-    if(!isset($_POST["add_cart"])) {
+      if(!isset($_POST["add_cart"])) {
 
-        $product_array = array(
-          'product_name'    => $_POST["naam"],
-          'product_price'   => $_POST["prijs"],
-          'product_ean'     => $_POST["EAN"],
-          'product_quantity'=> $_POST["aantal"],
-        );
-        $cart_data[] = $product_array;
-        $product_data = json_encode($cart_data);
-        setcookie('schoppingCart','$product_data', time() + (86400 * 30));
-        // header("location:product/detail.twig?success=1")
+          $product_array = array(
+            'product_name'    => $_POST["naam"],
+            'product_price'   => $_POST["prijs"],
+            'product_ean'     => $_POST["EAN"],
+            'product_quantity'=> $_POST["aantal"],
+          );
+          $cart_data[] = $product_array;
+          $product_data = json_encode($cart_data);
+          setcookie('schoppingCart','$product_data', time() + (86400 * 30));
+          // header("location:product/detail.twig?success=1")
 
-    }
-  //   if(!isset($_GETS["success"])) {
-  //     $message = 'gelukt ?';
-  // }
-
+      }
+    //   if(!isset($_GETS["success"])) {
+    //     $message = 'gelukt ?';
+    // }
+  }
 }
-
-
-
-?>
